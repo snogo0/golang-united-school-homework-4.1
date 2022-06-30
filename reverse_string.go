9229f41c8b3e43b15e2b16ca3759f6f5f33cc559
Alex M <snogo@e1.ru>
2022-06-30 16:52:58 +0500

    Implement string reversing with runes

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,12 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	runes := []rune(input)
+	size := len(runes)
+	runesRev := make([]rune, size)
+	for i := size; i > 0; i-- {
+		runesRev[size-i] = runes[i-1]
+	}
+	output = string(runesRev)
 	return output
 }
